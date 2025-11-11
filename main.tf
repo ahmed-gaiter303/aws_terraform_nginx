65cc2af64e96ed33b8a6314ba9627063886ea880
Ahmed Ekramy <ahmedekramy303@gmail.com>
2025-11-12 01:07:12 +0200

    Add Terraform configuration for Nginx on AWS EC2
    This commit adds a complete Terraform configuration to deploy an Nginx server on AWS EC2, including VPC, subnet, security group, and EC2 instance setup.

--- main.tf
+++ main.tf
@@ -1,19 +1,81 @@
-provider "aws" {
-  region = "us-east-1"
+########################################################
+# Terraform - Deploy Nginx on AWS EC2
+# Includes VPC, Subnet, Security Group, EC2 instance
+########################################################
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu)
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 }
 
-resource "aws_instance" "nginx" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+# VPC
+resource "aws_vpc" "tf_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "tf-demo-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "tf_subnet" {
+  vpc_id            = aws_vpc.tf_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags = {
+    Name = "tf-demo-subnet"
+  }
+}
+
+# Security Group
+resource "aws_security_group" "nginx_sg" {
+  name        = "nginx-sg"
+  description = "Allow HTTP inbound"
+  vpc_id      = aws_vpc.tf_vpc.id
+
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nginx-sg"
+  }
+}
+
+# EC2 Instance with nginx installation
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.tf_subnet.id
+  vpc_security_group_ids      = [aws_security_group.nginx_sg.id]
+  associate_public_ip_address = true
+  key_name                    = length(trim(var.ssh_key_name)) > 0 ? var.ssh_key_name : null
 
   user_data = <<-EOF
               #!/bin/bash
-              sudo apt update
-              sudo apt install -y nginx
-              sudo systemctl start nginx
+              apt-get update -y
+              apt-get install -y nginx
+              systemctl enable nginx
+              systemctl start nginx
+              echo "Hello from Terraform-built Nginx" > /var/www/html/index.html
               EOF
 
   tags = {
-    Name = "Terraform-Nginx"
+    Name = "tf-nginx-instance"
   }
 }
